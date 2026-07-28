f1a95f396154af5f9983356747f9639b154c5c70
simranchauhan13 <simranchauhan1394@gmail.com>
2026-07-28 13:56:37 +0530

    Prepare frontend for deployment

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,9 +1,12 @@
 import { ChangeEvent, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { SignupInput } from "@simranchauhan/medium-common";
 import axios from "axios";
 import { BACKEND_URL } from "../config";
-
+type SignupInput = {
+  username: string;
+  password: string;
+  name?: string;
+};
 export const Auth = ({ type }: { type: "signup" | "signin" }) => {
     const navigate = useNavigate();
     const [postInputs, setPostInputs] = useState<SignupInput>({
--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,1 +1,1 @@
-export const BACKEND_URL = "http://localhost:5000";
+export const BACKEND_URL = "https://blogcraft-backend-aqdx.onrender.com";
